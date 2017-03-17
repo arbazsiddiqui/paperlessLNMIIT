69cf181e22ad4120e5946df3b5538be194a1cfd7
Arpit Banerjee <arpitbanerjee95@gmail.com>
2017-03-18 02:53:50 +0530

    updated application types

--- app/models/student.js
+++ app/models/student.js
@@ -3,6 +3,8 @@ var mongoose = require('mongoose');
 
 var studentSchema = mongoose.Schema({
     rollNumber: String,
+    email :  String,
+    batch : String,
     grades: mongoose.Schema.Types.Mixed,
     app_id: [{sent:[String],received:[String]}]
 
